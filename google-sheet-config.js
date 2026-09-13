175e5f39511de341499b0a4db2b199c158dbd644
Regina María Flores González <a00838345@tec.mx>
2026-09-13 15:43:05 -0600

    Update google-sheet-config.js

--- google-sheet-config.js
+++ google-sheet-config.js
@@ -11,7 +11,7 @@
 // Also, you must Share the sheet so that anyone with a link can access it
 //     Share button at top right of sheet -> General Access -> Anyone with the link -> Viewer
 // prettier-ignore
-export const googleSheetURL = "https://docs.google.com/spreadsheets/d/17sHlHcOilG9UmRju8YDGx4bRMIDpQ5Bpfzc0QI-Np6c";
+export const googleSheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQfAAdA3LekbCLyZV0-WZ-F6-GxfsNpgkzktMPorVKiaEB0TBT3xPsTn5_CNQ0ayegWXr3XPaQ1S6wE/pubhtml";
 
 // An API Key is required to read a google sheet from an application. The one below is for this version
 // of the application, you will need to generate your own key if you plan to publish this scrolly story on
